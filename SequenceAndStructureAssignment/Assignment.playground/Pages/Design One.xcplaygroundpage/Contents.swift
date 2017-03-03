b325dda644b1dd54a86a821801d39952d7d08294
Student <student@macbook-pro-0863-sebastian-danson.local>
2017-03-03 10:50:22 -0500

    started making the other diagonal lines

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -36,27 +36,44 @@ for x in stride(from: 25, through: 475, by: 50){
         canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
     }
 }
-for X in stride(from: 0, through: 500, by: 16) {
-            canvas.drawLine(fromX: 0, fromY: X, toX: X, toY: 0)
+for X in stride(from: 0, through: 500, by: 20) {
+    canvas.drawLine(fromX: 0, fromY: X, toX: X, toY: 0)
     //choose the color
     if X % 1 == 0 {
-        canvas.defaultLineWidth = 6
+        canvas.defaultLineWidth = 7
     }else{
         canvas.defaultLineWidth = 1
     }
 }
-for Y in stride(from: 11, through: 500, by: 16){
+for Y in stride(from: 20, through: 500, by: 20){
     
     
     //choose the color
     if Y % 1 == 0 {
-        canvas.defaultLineWidth = 6
+        canvas.defaultLineWidth = 7
     }else{
         canvas.defaultLineWidth = 1
     }
-
+    
     canvas.drawLine(fromX: Y, fromY: 500, toX: 500, toY: Y)
 }
+
+
+for  i in stride(from: 0, through: 24, by: 3){
+    if i % 6 == 0 {
+        canvas.defaultLineWidth = 7
+        canvas.lineColor = Color.black
+    } else {
+        canvas.lineColor = Color.white
+        
+    }
+    canvas.drawLine(fromX: 10+i*2, fromY: 400-i, toX: 75+i*2, toY: 500-i)
+    canvas.drawLine(fromX: 10+i*2, fromY: 300-i, toX: 75+i*2, toY: 400-i)
+    canvas.drawLine(fromX: 10+i*2, fromY: 200-i, toX: 75+i*2, toY: 300-i)
+    canvas.drawLine(fromX: 10+i*2, fromY: 100-i, toX: 75+i*2, toY: 200-i)
+    canvas.drawLine(fromX: 10+i*2, fromY: 0-i, toX: 75+i*2, toY: 100-i)
+}
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
